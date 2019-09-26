7f53a42a6fa4c6320ea3253f7d97f80e192570d1
idmyn <hello@davidmyno.rs>
2019-09-26 13:54:04 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,28 +2,58 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new
+    row[2] = row[2].to_i
+    student.id, student.name, student.grade = row
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+    result = DB[:conn].execute(sql)
+    result.map { |row| new_from_db(row) }
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+    SQL
+    result = DB[:conn].execute(sql, name).first
+    new_from_db(result)
   end
-  
+
+  def self.all_students_in_grade_9
+    all_students_in_grade_X(9)
+  end
+
+  def self.students_below_12th_grade
+    all.select { |student| student.grade < 12 }
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    all.select { |student| student.grade.eql?(10) }.first(x)
+  end
+
+  def self.first_student_in_grade_10
+    all.find { |student| student.grade.eql?(10) }
+  end
+
+  def self.all_students_in_grade_X(x)
+    all.select { |student| student.grade.eql?(x) }
+  end
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql, name, grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
